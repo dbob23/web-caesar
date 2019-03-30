8c0b161a5080f874328e4e0b4e3ae7ba3ac7683d
Dennis Segall <dennis.segall@sbcglobal.net>
2019-03-30 09:34:30 -0500

    Finish web-caesar

--- main.py
+++ main.py
@@ -10,32 +10,30 @@ form = """
     <html>
         <head>
             <style>
-                form {
+                form {{
                     background-color: #eee;
                     padding: 20px;
                     margin: 0 auto;
                     width: 540px;
                     font: 16px sans-serif;
                     border-radius: 10px;
-                }
+                }}
                 
-                textarea {
+                textarea {{
                     margin: 10px 0;
                     width: 540px;
                     height: 120px;
-                }
+                }}
             </style>
         </head>
         </body>
-            <form action="/rotate" method="post">
-                <label for="rotate-by">
+            <form action="/" method="POST">
+                <label for="rot">
                     Rotate by:
                     <input type="text" id="rot" name="rot" value="0"/>
                 </label>
-                <label for="/text" method="post">
-                    <textarea id="text" name="text"/>
-                    </textarea>
-                </label>
+                <textarea id="text" name="text"/>{0}</textarea>
+
                 <input type="submit" value="Submit Query"/>
 
         </body>
@@ -45,15 +43,15 @@ form = """
 
 @app.route("/", methods=['POST'])
 def encrypt():
-    rot_element = int(rot)
-    text_element = text
-    encrypted_element = rotate_string(text_element)
+    rot = int(request.form["rot"])
+    text = request.form["text"]
+    encrypted_string = rotate_string(text, rot)
 
-    return "<h1>" + encrypted_element + "</h1>"
+    return form.format(encrypted_string)
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 app.run()
 
